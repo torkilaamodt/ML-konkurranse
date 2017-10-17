9ab5e7f7eceabf3bc011bc0549c57d063ebdaf9f
Birgitte Haavardsholm <birgitte.haavardsholm@gmail.com>
2017-10-17 20:30:25 +0200

    Endrer datasett på sckitilearn

--- scikitlearn/train.py
+++ scikitlearn/train.py
@@ -6,9 +6,9 @@ from math import sqrt
 
 X = []
 y = []
-
+path = '../data/Treningsdata/normalisert_training_data_without_header.csv'
 def readDataSet():
-    with open('../data/Treningsdata/normalisert_training_data_without_header.csv', 'rU') as csvfile:
+    with open(path, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in reader:
                 row = list(map(lambda x: float(x), row))
@@ -24,8 +24,31 @@ mlp.fit(X, y)
 
 print("Training set score: %f" % mlp.score(X, y))
 
-
+# Root mean squeare error
 p = mlp.predict(X);
 mse = MSE( y, p )
 rmse = sqrt( mse )
 print rmse
+
+#-------------------------------------------------------
+# Skriv resultat til fil
+
+X_test = []
+y_test = []
+
+def readTestSet():
+    with open('../data/Testdata/normalisert_test_data_without_header.csv', 'rU') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in reader:
+                row = list(map(lambda x: float(x), row))
+                X_test.append(row)
+
+
+readTestSet()
+
+p = mlp.predict(X_test);
+
+fileOut = open("../denormalise/input", "w")
+
+for prediction in p:
+    fileOut.write("{0}\n".format(prediction))
